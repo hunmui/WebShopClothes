c0ab51947f1e15c3bd08f396460c0b72fe4162ff
hung <hung71718@gmail.com>
2022-04-28 23:07:34 +0700

    add layout Admin

---
+++ WebShopBanQuanAo/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebShopBanQuanAo.Areas.Admin.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Admin/Products
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        public ActionResult DetailProduct()
+        {
+            return View();
+        }
+        public ActionResult ListProduct()
+        {
+            return View();
+        }
+        public ActionResult GridProduct()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebShopBanQuanAo/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebShopBanQuanAo.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult AccountVerified()
+        {
+            return View();
+        }
+        public ActionResult Customers()
+        {
+            return View();
+        }
+        public ActionResult LookScreen()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult ResetPassword()
+        {
+            return View();
+        }
+    }
+}
